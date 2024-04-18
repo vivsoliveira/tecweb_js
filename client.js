dfca6026331c49a3f61528fae0f4ea7e612b4489
vivsoliveira <victoriaof@al.insper.edu.br>
2024-04-17 21:57:51 -0300

    ex1 feito

--- client.js
+++ client.js
@@ -1,22 +1,60 @@
 const axios = require("axios");
-const config = {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpY3Rvcmlhb2YiLCJ0ZW50YXRpdmEiOjMsImlhdCI6MTcxMzMxMjAwOCwiZXhwIjoxNzEzMzEyMDY4fQ.u4r0GEj1UMARIcVeHAjnVT5yMuGjApW7t6z9pU6uoz4' 
-    }
-};
-axios.post('https://tecweb-js.insper-comp.com.br/token', {
-    username: "victoriaof"
-}, config)
-.then((response) => {
-    console.log('Token recebido:', response.data);
-    config.headers.Authorization = `Bearer ${response.data.accessToken}`;  // Atualizando o token no cabeçalho
-    return axios.get('https://tecweb-js.insper-comp.com.br/exercicio', config);
-})
-.then((exercisesResponse) => {
-    console.log('Exercícios recebidos:', exercisesResponse.data);
-})
-.catch((error) => {
-    console.error('Erro na requisição:', error);
-});
+
+// Função para obter um novo token
+async function fetchToken() {
+    const response = await axios.post('https://tecweb-js.insper-comp.com.br/token', {
+        username: "victoriaof"
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    });
+    console.log("Token obtido:", response.data.accessToken);
+    return response.data.accessToken;
+}
+
+// Função para buscar exercícios usando o token atualizado
+async function fetchExercises(token) {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+    const response = await axios.get('https://tecweb-js.insper-comp.com.br/exercicio', config);
+    return response.data;
+}
+
+async function main() {
+    const token = await fetchToken();  // Obter token
+    const exercises = await fetchExercises(token);  // Obter exercícios
+
+    console.log("Dados dos exercícios:", exercises);
+
+    // Supondo que a estrutura dos dados de exercício seja conhecida e consistente
+    let a = exercises.soma.entrada.a;
+    let b = exercises.soma.entrada.b;
+    let resultadoSoma = soma(a, b);
+    console.log("Resultado da soma:", resultadoSoma);
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    axios.post(`https://tecweb-js.insper-comp.com.br/exercicio/soma`, {
+        resposta: resultadoSoma
+    }, config)
+    .then(response => console.log("Resposta do servidor:", response.data))
+}
+
+function soma(a, b) {
+    return a + b;
+}
+
+main();
